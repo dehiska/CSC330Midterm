1750ecee6915cdbb9bcda49fae02d3f652dc48ea
dehiska <denissoulima@gmail.com>
2024-03-13 15:53:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 def add_nums(num1, num2):
     return f'<h1>{(num1)+(num2)} = {str(int(num1)+int(num2))}</h1>'
 
-@app.route('/subtract/<num1></num2>')
+@app.route('/subtract/<num1>/<num2>')
 def subtract_nums(num1, num2):
     return f'<h1>{(num1)-(num2)} = {str(int(num1)-int(num2))}</h1>'
 
-@app.route('/multiply/<num1></num2>')
+@app.route('/multiply/<num1>/<num2>')
 def multiply_nums(num1, num2):
     return f'<h1>{(num1)*(num2)} = {str(int(num1)*int(num2))}</h1>'
 
-@app.route('/divide/<num1></num2>')
+@app.route('/divide/<num1>/<num2>')
 def divide_nums(num1, num2):
     return f'<h1>{(num1)/(num2)} = {str(float(int(num1)+int(num2)))}</h1>'
 
